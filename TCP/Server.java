00214613856b207acbcc15651b58e55eb2e221a0
K Keshav <126003131@sastra.ac.in>
2024-11-06 19:41:23 +0530

    Delete TCP directory

--- TCP/Server.java
+++
@@ -1,9 +0,0 @@
-public class Server {
-    public static void main(String[] args) {
-        try {
-            TCPServer.main(args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
